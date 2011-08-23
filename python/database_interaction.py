c4d38adc1ccf81fb04defd6a48adb3f73ad3af35
Jason Kamphaugh <jkamph@juno.com>
2011-08-23 00:28:51 -0500

    Added further functions to be called.

--- python/database_interaction.py
+++ python/database_interaction.py
@@ -19,33 +19,33 @@ class Database_Interaction:
       to the database"""
       
       #get a handle to the spreadsheet client 
-      self.client  = gdata.spreadsheet.service.SpreadsheetsService()
+      self._client  = gdata.spreadsheet.service.SpreadsheetsService()
       
       #save the user name and password 
-      self.client.email = username
-      self.client.password = password
+      self._client.email = username
+      self._client.password = password
       
       #log in to the system 
-      self.client.ProgrammaticLogin()
+      self._client.ProgrammaticLogin()
 
       #save the name of the database      
-      self.database_name = database_name
+      self._database_name = database_name
 
-      self.database_sheet = None
+      self._database_sheet = None
 
       #get the list of spreadsheets
-      self.spreadsheet_list = self.client.GetSpreadsheetsFeed()
-      self.key = -1
+      self._spreadsheet_list = self.client.GetSpreadsheetsFeed()
+      self._key = -1
       
       #make sure we got a list
-      if self.spreadsheet_list <> None:
-          for sheet in self.spreadsheet_list.entry:
+      if self._spreadsheet_list <> None:
+          for sheet in self._spreadsheet_list.entry:
               
-              if database_name == sheet.title.text:
+              if database_name == sheet._title.text:
                   
-                  self.database_sheet = sheet
+                  self._database_sheet = sheet
                   path_parts = sheet.id.text.split( '/' )
-                  self.key = path_parts[len(path_parts) - 1]
+                  self._key = path_parts[len(path_parts) - 1]
               #end if database name matches
 
           #end loop through the database list
@@ -53,5 +53,48 @@ class Database_Interaction:
       #end if we got a spreadsheet list      
       
    #end init
+
+   def Get_Table( self,
+                  table_name ):
+      """This method will retrieve the table (tab) with the given name"""
+
+      #make sure we have a valid table name
+      if None <> table_name:
+
+         #retrieve the table from the spreadsheet
+
+      #end if valid table_name
+
+   #end Get_Table
+
+   def Get_Player( self,
+                   player_name ):
+      """This method will retrive the specified player."""      
+
+      #make sure that we received a valid player name
+      if None <>:
+
+         #get the player table
+         #maybe not if we already have that table? if we don't want to ping the
+         #spreadsheet each time, do we want to check that here or in the 
+         #Get_Table method?
+
+
+   #end Get_Player
+
+   def Get_Team( self,
+                 team_name ):
+      """This method will retrieve the team of the specified name."""
+
+      #near as I can tell, this is just a pass-through to get the table
+      #with the team name.
+      #We might want to beef this up so that it will pull all of the pertinent
+      #data (players, salary, etc) in to a structure to be read outside of
+      #this function.
+      return self.Get_Table( team_name )
+
+   #end Get_Team
+
+
 
 #end Database_Interaction   
