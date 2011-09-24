f49b7be6b39c638b93559f9221cae46fcee23c64
Jason Kamphaugh <jkamph@juno.com>
2011-09-24 12:18:09 -0500

    Adding functionality to the team class.

--- qt/Obscure-Reference/team.cpp
+++ qt/Obscure-Reference/team.cpp
@@ -7,18 +7,128 @@
   *
   */
 
-#include "or_common_defines.hpp"
+#include <QString>
+#include <QMap>
+#include <QMapIterator>
+
+#include "or_system_util.hpp"
 #include "team.hpp"
+#include "player.hpp"
 
 using namespace Obscure_Reference;
 
-Team::Team()
+Team::Team() :
+   m_name( QString::null ),
+   m_id( invalidSalary ),
+   m_totalSalary( 0 )
 {
 }/* end ::Team */
 
 Team::Team( QString name,
-            TeamId  id ) :
+            int     id ) :
    m_name( name ),
-   m_id( id )
+   m_id( id ),
+   m_totalSalary( 0 )
 {
 }/* end ::Team name & ID */
+
+
+void
+Team::addPlayer( Player* newPlayer )
+{
+   /* make sure that the player isn't already on the team */
+   if (!m_playerMap.contains( newPlayer->getId() ))
+   {
+
+      /* insert the player in to the map */
+      m_playerMap.insert( newPlayer->getId(), newPlayer );
+
+      /* add this player's salary to our total salary cap number */
+      m_totalSalary += newPlayer->getCurrentSalary( );
+
+   }/* end if the palyer isn't already on the team */
+
+}/* end ::addPlayer */
+
+Player*
+Team::getPlayerById( int playerId )
+{
+
+   Player* foundPlayer = NULL;
+
+   /* make sure that this player exists in the map */
+   if (m_playerMap.contains( playerId ))
+   {
+      /* get the reference to the player */
+      foundPlayer = m_playerMap.value( playerId );
+   }/* end if the player exists in the map */
+
+   return foundPlayer;
+
+}/* end ::getPlayerById */
+
+Player*
+Team::getPlayerByName( QString playerName )
+{
+   Player* foundPlayer = NULL;
+   Player* currentPlayer = NULL;
+
+   /* declare the iterator type for walking through the map */
+   QMapIterator<int, Player*> mapIter(m_playerMap);
+
+   /* loop through the map looking for the player with the specified
+    * name
+    */
+   while (mapIter.hasNext( ))
+   {
+      /* get the next item */
+      mapIter.next();
+
+      /* get the player at the current position */
+      currentPlayer = mapIter.value( );
+
+      /* check for a match */
+      if (playerName == currentPlayer->getName())
+      {
+         /* save the pointer to be returned */
+         foundPlayer = currentPlayer;
+
+         /* break out of the loop */
+         break;
+      }/* end if a match was found */
+   }/* end loop through the map */
+
+   return foundPlayer;
+}/* end ::getPlayerByName */
+
+
+void
+Team::dropPlayer( int playerId )
+{
+
+   /* make sure that the player is in the list */
+   if (m_playerMap.contains( playerId ))
+   {
+      /* get a reference to the player */
+      Player* removedPlayer = m_playerMap.value( playerId );
+
+      /* remove this player from our list */
+      m_playerMap.remove( playerId );
+
+      /* subtract this player's salary from our total */
+      m_totalSalary -= removedPlayer->getCurrentSalary( );
+   }
+}/* end ::dropPlayer */
+
+
+QString
+Team::getTeamName( void )
+{
+   return m_name;
+}/* end ::getTeamName */
+
+int
+Team::getSalary( void )
+{
+   return m_totalSalary;
+}/* end ::getSalary */
--- qt/Obscure-Reference/team.hpp
+++ qt/Obscure-Reference/team.hpp
@@ -8,8 +8,10 @@
   */
 
 #include <QString>
+#include <QMap>
 
-#include "or_common_defines.hpp"
+#include "or_system_util.hpp"
+#include "player.hpp"
 
 #ifndef TEAM_HPP
 #define TEAM_HPP
@@ -27,13 +29,89 @@ public:
      */
    Team();
 
+   /** This is the constructor for a new instance of the Team object.
+     *
+     * @param name  The name of the team.
+     *
+     * @param id    The ID of the team.
+     *
+     */
    Team( QString name,
-         TeamId  id );
+         int     id );
+
+   /** This method will add a player to the team's player mapping.
+     *
+     * @param newPlayer  The new player to be added to this team.
+     *
+     * @post  \a newPlayer has been added to this team.
+     *
+     */
+   void
+   addPlayer( Player* newPlayer );
+
+   /** This method will retrieve the reference to the player by the
+     * provided ID.
+     *
+     * @param playerId  The ID of the player for which we are searching.
+     *
+     * @return Reference to the player on the team that matches \a playerId,
+     *         if one exists. Null otherwise.
+     *
+     */
+   Player*
+   getPlayerById( int playerId );
+
+   /** This method will retrieve the reference to the player using the
+     * provided name.
+     *
+     * @param playerName The name of the player to retrieve.
+     *
+     * @return Reference tot he player on the team that matches \a playerName
+     *         if one exists. Null otherwise.
+     *
+     */
+   Player*
+   getPlayerByName( QString playerName );
+
+   /** This method will remove the specified player from the team.
+     *
+     * @param playerId  The ID of the player to be removed.
+     *
+     * @post The player corresponding to \a playerId has been removed from
+     *       the team.
+     *
+     */
+   void
+   dropPlayer( int playerId );
+
+   /** This method will retrieve the name of this team.
+     *
+     * @return The team name.
+     *
+     */
+   QString
+   getTeamName( void );
+
+   /** This method will retrieve the total salary cap number of this team.
+     *
+     * @return The total salary of all of the players currently on this team.
+     *
+     */
+   int
+   getSalary( void );
 
 private:
+   /** The name of the team */
    QString m_name;
 
-   TeamId  m_id;
+   /** The ID of the team */
+   int  m_id;
+
+   /** The current salary cap number of this team */
+   int m_totalSalary;
+
+   /** The list of players currently on this team */
+   QMap<int, Player*> m_playerMap;
 };
 
 }/* end namespace Obscure_Reference */
